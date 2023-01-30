da1b628a4b53342a0e4162f2aa6386b6f1492405
Ishit <ishitsingh2001@gmail.com>
2023-01-30 12:13:11 +0530

    chore : remove log statements from API's

--- Controller/tasks.js
+++ Controller/tasks.js
@@ -1,27 +1,20 @@
 const {getTasksService,postTasksService,deleteTasksService,getSingleTaskService,patchTaskService}=require('../Services/tasks');
 const getTasks=(req,resp)=>{
-  console.log('Get Tasks called');
   let tasks=getTasksService();
   resp.send(tasks);
 };
 const postTasksHandler=(req,resp)=>{
-  console.log('Post Tasks called');
   const thisTask=postTasksService(req,resp);
   resp.send(thisTask);
 };
 const deleteTasksHandler=(req,resp)=>{
-  console.log('Delete Tasks called');
   let tasks=deleteTasksService(req,resp);
   resp.send(tasks);
 };
 const getSingleTaskHandler=(req,resp)=>{
-  console.log('Get Single Task called');
-  return getSingleTaskService(req,resp);
-  //let indexOfObj=tasks.findIndex((task)=>task.id==req.params.id);
-    
+  return getSingleTaskService(req,resp); 
 };
 const patchTaskHandler=(req,resp)=>{
-  console.log('Patch Tasks called');
   return patchTaskService(req,resp); 
 };
 module.exports={
--- Services/tasks.js
+++ Services/tasks.js
@@ -1,7 +1,6 @@
 const uuid=require('uuid');
 let tasks=[];
 const getTasksService=()=>{
-  console.log('Get Tasks Service called');
   return tasks;
 };
 const postTasksService=(req)=>{
@@ -9,16 +8,13 @@ const postTasksService=(req)=>{
   recievedData['id'] = uuid.v4();
   recievedData['isComplete'] = false; 
   tasks.push(recievedData);
-  console.log('Get Tasks Service called');
   return recievedData;
 };
 const deleteTasksService=()=>{
-  console.log('Delete Tasks Service called');
   tasks=tasks.filter(task => task.isComplete==false);
   return tasks;
 };
 const getSingleTaskService=(req,resp)=>{
-  console.log('Get Single Task Service called');
   let indexOfObj=tasks.findIndex((task)=>task.id==req.params.id);
   if(indexOfObj==-1){
     return resp.status(404).send('No tasks found');
