56c348dcb8c1e8db727eaa2c4d2f1cc1c5d293d8
Ishit <ishitsingh2001@gmail.com>
2023-01-30 12:05:35 +0530

    chore : remove redundant comments

--- Routes/taskRoutes.js
+++ Routes/taskRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {getTasks,postTasksHandler,deleteTasksHandler,getSingleTaskHandler,patchTaskHandler}=require('../Controller/tasks');
 
+
 router.get('/tasks', getTasks);
 router.post('/tasks', postTasksHandler);
 router.delete('/tasks/isComplete=true?',deleteTasksHandler);
-
 router.get('/tasks/:id',getSingleTaskHandler);
 router.patch('/tasks/:id',patchTaskHandler);
 
